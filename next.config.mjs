8d950cdac3c7697f9f710f8d710c2db7c301f983
huangtinghao <a123>
2024-05-04 14:15:27 +0800

    fix: build config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    distDir: 'dist',
+    output: 'export', // 导出静态文件、如不在此处配置,则需要另外添加 next export 脚本
+};
 
 export default nextConfig;
