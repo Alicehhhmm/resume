d893abdd1a7c0deba068fd365df1cfd9d2f629ec
huangtinghao <a123>
2024-05-04 14:42:37 +0800

    fix: usr ACCESS_TOKEN

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
     distDir: 'dist',
     output: 'export', // 导出静态文件、如不在此处配置,则需要另外添加 next export 脚本
+    reactStrictMode: true, // 使用React严格模式
 };
 
 export default nextConfig;
