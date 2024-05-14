b9528c5f5e65bd4ccccb3a7ed388bc8a60bda286
huangtinghao <a123>
2024-05-14 17:06:23 +0800

    fix：tailwind use add lib

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
-export default function Home() {
-  return (
-    <main className="">
-      <h1>Home</h1>
-    </main>
-  );
+import Link from "next/link"
+import { siteConfig } from "@/commont/site-config"
+
+export default function IndexPage() {
+    return (
+        <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+            <div className="flex max-w-[980px] flex-col items-start gap-2">
+                <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+                    Beautifully designed components{" "}
+                    <br className="hidden sm:inline" />
+                    built with Radix UI and Tailwind CSS.
+                </h1>
+                <p className="max-w-[700px] text-lg text-muted-foreground">
+                    Accessible and customizable components that you can copy and
+                    paste into your apps. Free. Open Source. And Next.js 14
+                    Ready.
+                </p>
+            </div>
+            <div className="flex gap-4">
+                <Link
+                    href={siteConfig.links.docs}
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    Documentation
+                </Link>
+                <Link
+                    target="_blank"
+                    rel="noreferrer"
+                    href={siteConfig.links.github}
+                >
+                    GitHub
+                </Link>
+            </div>
+        </section>
+    )
 }
---
+++ src/commont/site-config.ts
@@ -0,0 +1,17 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+    name: "resume",
+    description: "Beautifully resume built with Rose UI and Tailwind CSS.",
+    mainNav: [
+        {
+            title: "Home",
+            href: "/",
+        },
+    ],
+    links: {
+        twitter: "https://github.com/Alicehhhmm/resume",
+        github: "https://github.com/Alicehhhmm/resume",
+        docs: "https://github.com/Alicehhhmm/resume",
+    },
+}
