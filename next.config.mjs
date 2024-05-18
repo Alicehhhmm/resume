d25e93523b689ab26bb0c1b78c0376da42c0756c
huangtinghao <a123>
2024-05-18 11:25:32 +0800

    fix：env pro

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '', // 确保静态文件路径为相对路径
+    basePath: isProd ? '/blog' : '', // 确保静态文件路径为相对路径
     distDir: 'dist',
     output: 'export', // 导出静态文件、如不在此处配置,则需要另外添加 next export 脚本
     reactStrictMode: true, // 使用React严格模式
+    assetPrefix: "/assets", // 静态资源前缀
 };
 
 export default nextConfig;
