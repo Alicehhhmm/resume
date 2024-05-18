cf83a8990461b94f3a42a277b477cb7d1f11e33a
huangtinghao <a123>
2024-05-18 11:02:26 +0800

    fix：build

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: '', // 确保静态文件路径为相对路径
     distDir: 'dist',
     output: 'export', // 导出静态文件、如不在此处配置,则需要另外添加 next export 脚本
     reactStrictMode: true, // 使用React严格模式
