8dfcd56cb4f9cf6dd621163569c75b91fea6d5bc
huangtinghao <a123>
2024-06-03 10:27:29 +0800

    feat: add cmp PostCard

---
+++ src/components/PostCard.tsx
@@ -0,0 +1,21 @@
+import { Post } from "contentlayer/generated"
+import Link from "next/link"
+import { format, parseISO } from "date-fns"
+
+export function PostCard(post: Post) {
+  return (
+    <div className="mb-4 p-2 hover:bg-slate-100 rounded-md">
+      <Link href={post.url} className="text-blue-700 hover:text-blue-900 dark:text-blue-400" passHref legacyBehavior>
+        <div className="flex-nowrap flex justify-center items-center cursor-pointer">
+          <h2 className="text-md text-neutral-800">{post.title}</h2>
+          <span className="grow mx-4 h-1 border-b border-dashed border-line" />
+          <time dateTime={post.date} className="block text-sm text-[#8a8f8d]">
+            <a title={format(parseISO(post.date), "LLLL d, yyyy, h:mm a ").concat("CST + 8")}>{format(parseISO(post.date), "yyyy-MM-dd hh:mm")}</a>
+          </time>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default PostCard
